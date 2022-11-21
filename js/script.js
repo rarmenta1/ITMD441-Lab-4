144e1b025e079268d786b8bfe479304eca241e84
Rafael Armenta <rarmenta@hawk.iit.edu>
2022-11-21 04:42:58 -0600

    Update script.js
    javascript accomplished the following:
     - successfully used weather API
     - successfully displayed data from the API's JSON

---
+++ js/script.js
@@ -0,0 +1,66 @@
+function returnLocation(){
+  let input = document.getElementById("location_name_field").value;
+
+  alert(input)
+  
+  var myRequest = new XMLHttpRequest();
+  myRequest.onreadystatechange = function(){
+  if (myRequest.readyState === 4) {
+  if (myRequest.status === 200)
+  var myArray = JSON.parse(myRequest.responseText);
+  parseData(myArray);
+  }
+  }
+  
+  let site = myRequest.open('GET', 'https://weatherdbi.herokuapp.com/data/weather/' + input, true);
+  
+  let test1 = JSON.stringify(site);
+  
+  myRequest.send();
+
+  function parseData(arr) {
+
+  console.log(arr);
+
+  usrRegion = arr.region;
+  usrDayHr = arr.currentConditions.dayhour;
+  usrTempCel = arr.currentConditions.temp.c;
+  usrTempFah = arr.currentConditions.temp.f;
+  usrPrecip = arr.currentConditions.precip;
+  usrHumidity = arr.currentConditions.humidity;
+  usrWindKM = arr.currentConditions.wind.km;
+  usrWindMile = arr.currentConditions.wind.mile;
+  usrComment = arr.currentConditions.comment;
+  usrIcon = arr.currentConditions.iconURL;
+
+  console.log(arr.region);
+  console.log(arr.currentConditions);
+  console.log(arr.currentConditions.dayhour);
+  console.log(arr.currentConditions.temp);
+  console.log(arr.currentConditions.precip);
+  console.log(arr.currentConditions.humidity);
+  console.log(arr.currentConditions.wind);
+  console.log(arr.currentConditions.comment);
+
+
+
+  
+  const icon = document.querySelector("#weather-icon");
+
+  document.getElementById('Region').innerHTML = 'Region: ' + usrRegion;
+  document.getElementById('DayHour').innerHTML = 'Time: ' + usrDayHr;
+  document.getElementById('Temp').innerHTML = 'Temperature: ' + usrTempCel + "\u2103/" + usrTempFah + 	"\u2109";
+  document.getElementById('Precip').innerHTML = 'Precipitation: ' + usrPrecip;
+  document.getElementById('Humidity').innerHTML = 'Humidity: ' + usrHumidity;
+  document.getElementById('Wind').innerHTML = 'Wind: ' + usrWindKM + "km/" + usrWindMile + "mi";
+  document.getElementById('Comment').innerHTML = 'Comment: ' + usrComment;
+  icon.src = usrIcon;
+
+
+
+
+  }
+
+
+
+}
